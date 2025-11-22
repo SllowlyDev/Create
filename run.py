435cce311ea1efd20e7327b7de05037dd152fd0f
SllowlyXynraa <145588734+SllowlyDev@users.noreply.github.com>
2025-11-22 23:39:37 +0700

    Update run.py

---
+++ run.py
@@ -0,0 +1,20 @@
+import os, sys, requests
+
+def Mulai():
+	try:
+		os.system("clear")
+		print("Hello Anak Pungut. From SllowlyDev🖕")
+	except:
+		pass
+
+if __name__ == "__main__":
+	try:
+		os.system("clear")
+		print("Wait For a minute")
+		time.sleep(2)
+		print("Check Your Files Data")
+		time.sleep(2)
+		os.system("git pull")
+		Mulai()
+	except:
+		pass
